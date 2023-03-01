329a869e981f96b81125b01569022ca7e1ae4b42
LuisRdz224 <luisrdzh224@gmail.com>
2023-03-01 11:58:56 -0600

    Mini project: Calculator

---
+++ calculadora/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Mini Calculadora';
+  numero1 = 0;
+  numero2 = 0;
+  resultado = 0;
+
+  sumar(): void {
+    console.log('la funcion fue llamada');
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  restar(): void {
+    this.resultado = this.numero1 - this.numero2;
+  }
+
+  multiplicar(): void {
+    this.resultado = this.numero1 * this.numero2;
+  }
+}
