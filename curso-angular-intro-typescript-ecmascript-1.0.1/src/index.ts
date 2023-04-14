69157f7e3da712571fde0201a36afb3cd3d3f883
LuisRdz224 <luisrdzh224@gmail.com>
2023-04-14 15:25:02 -0600

    Cap 17: Desestructuracion de Objetos

--- curso-angular-intro-typescript-ecmascript-1.0.1/src/index.ts
+++ curso-angular-intro-typescript-ecmascript-1.0.1/src/index.ts
@@ -1,7 +1,8 @@
 // import './topics/01-basic-types'
 // import './topics/02-object-interface'
 // import './topics/03-functions'
-import './topics/04-homework-types'
+// import './topics/04-homework-types'
+import './topics/05-basic-desestructuration'
 
 console.log('Hola Mundo desde VSCode')
 
---
+++ curso-angular-intro-typescript-ecmascript-1.0.1/src/topics/05-basic-desestructuration.ts
@@ -0,0 +1,26 @@
+interface AudioPlayer {
+    audioVolume: number
+    songDuration: number
+    song: string
+    details: Details
+}
+
+interface Details {
+    author: string
+    year: number
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolume: 90,
+    songDuration: 36,
+    song: 'La Durango',
+    details: {
+        author: 'Dharius',
+        year: 2020
+    }
+}
+
+const { song, details } = audioPlayer
+const { author } = details
+console.log('song: ' + song)
+console.log('Author: ' + author)
