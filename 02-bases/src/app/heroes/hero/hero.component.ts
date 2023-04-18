5da6825d165983b4c74414147be1354819e4b846
LuisRdz224 <luisrdzh224@gmail.com>
2023-04-18 13:00:34 -0600

    Cap 42 y 43: One way data binding

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   nombre: string = 'Luis';
   edad: number = 25;
+
+  get getCapitalizedName(): string {
+    return this.nombre.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.nombre} - ${this.edad}`;
+  }
+
+  changeName(name: string): void {
+    this.nombre = name;
+  }
+  changeEdad(age: number): void {
+    this.edad = age;
+  }
 }
