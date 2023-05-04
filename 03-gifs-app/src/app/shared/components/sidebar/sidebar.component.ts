4ccd5685e603052bb94593d36b07c68c058247ac
LuisRdz224 <180563@upslp.edu.mx>
2023-05-04 13:27:33 -0600

    Cap 93: Mostrar gifs

--- 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
+++ 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
@@ -7,10 +7,17 @@ import { GifsService } from '../../../gifs/services/gifs.service';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  @ViewChild('btnHistory')
+  public txtHistory!: ElementRef<HTMLInputElement>
 
   constructor(private gifsService: GifsService) { }
 
   get tags() {
     return this.gifsService.tagsHistory
+  }
+
+  sendToSearch(tag: string) {
+    console.log({ tag });
+    this.gifsService.searchTag(tag)
   }
 }
