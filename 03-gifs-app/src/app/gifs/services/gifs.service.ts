bf84a43090c3434159d3c2bb84b58d1bf778449a
LuisRdz224 <180563@upslp.edu.mx>
2023-05-04 11:40:55 -0600

    Cap 90: Realizar peticion HTTP

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,8 +7,10 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = []
+  private apiKey: string = 'A4kbzj0C9QDXKopPXI7v96ly76WwS09e';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory]
@@ -23,8 +26,22 @@ export class GifsService {
     this._tagsHistory = this.tagsHistory.splice(0, 10)
   }
 
-  searchTag(tag: string) {
+  searchTag(tag: string): void {
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
     if (tag.length === 0) return
     this.organizeHistory(tag)
+    // const res = await fetch('https://api.giphy.com/v1/gifs/search?api_key=A4kbzj0C9QDXKopPXI7v96ly76WwS09e&limit=10&q=Overwatch')
+
+
+    const res = this.http.get(`${this.serviceUrl}/search`, { params })
+    res.subscribe(response => {
+      console.log(response);
+
+    })
+
+
   }
 }
