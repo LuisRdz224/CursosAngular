f504598a7835011430cf10d333b447db4ab3b51f
LuisRdz224 <180563@upslp.edu.mx>
2023-05-04 10:47:08 -0600

    Cap 87: GifsService

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -12,9 +13,16 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>
 
+  constructor(private gifsService: GifsService) { }
+
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
     console.log({ newTag });
+
+    this.gifsService.searchTag(newTag)
+
+    this.tagInput.nativeElement.value = ''
   }
+
 
 }
