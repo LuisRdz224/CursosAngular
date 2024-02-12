bc6ad3f301d9f1f01ee3fc23bb7eb03f3c26fa89
LuisRdz224 <luisrdzh224@gmail.com>
2024-02-12 11:57:31 -0600

    Cap: 117 RouterLinks y RouterLinkOptions

--- 04-Country-SPA/CountryApp/src/app/shared/shared.module.ts
+++ 04-Country-SPA/CountryApp/src/app/shared/shared.module.ts
@@ -1,21 +1,30 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { AboutPageComponent } from './components/about-page/about-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
+    AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
-    AboutPageComponent
+    SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
+    AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
-    AboutPageComponent
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
