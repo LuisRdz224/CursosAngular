7b1329a7f8e330a03fcd470edf3c78da044e8c24
LuisRdz224 <luisrdzh224@gmail.com>
2024-02-13 09:53:55 -0600

    Cap 123: Mostrar paises con Observable y suscribe

--- 04-Country-SPA/CountryApp/src/app/app.module.ts
+++ 04-Country-SPA/CountryApp/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     SharedModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
