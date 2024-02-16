1220311659657547c0440cfe916787ca97e0ad7c
LuisRdz224 <luisrdzh224@gmail.com>
2024-02-16 09:05:00 -0600

    Cap 130: CatchError, argumentos como URL y SwitchMap

--- 04-Country-SPA/CountryApp/src/app/countries/services/countries.service.ts
+++ 04-Country-SPA/CountryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, catchError, map, of, tap } from "rxjs";
 import { Country } from '../interfaces/country';
 
 
@@ -15,10 +15,47 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+  searchCountryByAlphacode(term: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${term}`
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(error => {
+          console.log(error);
+          return of(null)
+        })
+      )
+  }
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => {
+          console.log(error);
+          return of([])
+        })
+      )
+  }
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => {
+          console.log(error);
+          return of([])
+        })
+      )
+  }
+  searchRegion(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${term}`
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => {
+          console.log(error);
+          return of([])
+        })
+      )
   }
 
 }
